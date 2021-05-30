cc0762508b58377d67f530a55f83ba25128f1d90
Sumit Kumar <sumit.k9887@gmail.com>
2021-05-30 20:46:29 +0530

    added windows support

---
+++ winuwp/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Flutter_Projects\\flutter_catalog" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter"
+  "PROJECT_DIR=E:\\Flutter_Projects\\flutter_catalog"
+  "FLUTTER_ROOT=E:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Flutter_Projects\\flutter_catalog\\winuwp\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Flutter_Projects\\flutter_catalog"
+  "FLUTTER_TARGET=E:\\Flutter_Projects\\flutter_catalog\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Flutter_Projects\\flutter_catalog\\.dart_tool\\package_config.json"
+)
